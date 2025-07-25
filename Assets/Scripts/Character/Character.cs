dee4223e233ee2c8175e047753fc0debe0078877
Ben Alexander <ben250402@yahoo.co.uk>
2025-07-25 19:41:47 +0100

    1 Added character health

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 
@@ -5,6 +6,29 @@ namespace Assets.Scripts.Character
 {
 	public class Character : MonoBehaviour
 	{
-		
+		public uint currentHealth;
+
+		[Header("Health")]
+		[SerializeField]
+		private uint maxHealth;
+
+
+		private void Awake()
+		{
+			currentHealth = maxHealth;
+		}
+
+		public void TakeDamage(uint value)
+		{
+			currentHealth = Math.Max(currentHealth - value, 0);
+		}
+
+		public void Heal(uint value)
+		{
+			currentHealth = Math.Min(currentHealth + value, maxHealth);
+		}
+
+		public bool IsDead()
+			=> currentHealth == 0;
 	}
 }
