b3ce281face81364df2df832036b37a3e3820bcc
Ben Alexander <ben250402@yahoo.co.uk>
2025-08-03 15:49:39 +0100

    3 Added finding first friend

---
+++ Assets/Scripts/Trigger/TeleportGameObjectsTriggerZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.Scripts.Trigger
+{
+	public class TeleportGameObjectsTriggerZone : PlayerHitTriggerZone
+	{
+		[SerializeField]
+		private List<GameObject> gameObjects;
+
+
+		protected override void Trigger()
+		{
+			foreach (var obj in gameObjects)
+			{
+				obj.transform.position = transform.position;
+			}
+		}
+
+		protected override void ExitTrigger()
+		{
+			// Nothing
+		}
+	}
+}
