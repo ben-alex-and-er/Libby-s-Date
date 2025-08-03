ed55c00cdadd46624490d6877ecf789a6425eae1
Ben Alexander <ben250402@yahoo.co.uk>
2025-08-03 11:22:32 +0100

    3 Added simple fog controlled by Y value

---
+++ Assets/Scripts/Scenes/Scotland/YControlledFog.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+
+namespace Assets.Scripts.Scenes.Scotland
+{
+	public class YControlledFog : MonoBehaviour
+	{
+		[SerializeField]
+		private float yStart = 100f;
+
+		[SerializeField]
+		private float yEnd = 200f;
+
+
+		[SerializeField]
+		private Vector2 startScale;
+
+		[SerializeField]
+		private Vector2 endScale;
+
+
+		[SerializeField]
+		private Transform fog;
+
+		[SerializeField]
+		private Transform player;
+
+		private void Awake()
+		{
+			fog.gameObject.SetActive(true);
+		}
+
+
+		private void Update()
+		{
+			var percentageThrough = (player.position.y - yStart) / (yEnd - yStart);
+
+			var newSize = percentageThrough switch
+			{
+				< 0 => startScale,
+				> 1 => endScale,
+				_ => (endScale - startScale) * percentageThrough + startScale,
+			};
+
+			fog.localScale = newSize;
+		}
+	}
+}
