8c4b9153ca6896b767435d6c3ef7fe75a3e0ec4f
Ben Alexander <ben250402@yahoo.co.uk>
2025-08-03 13:34:04 +0100

    3 Added speech for first part of scotland

---
+++ Assets/Scripts/Trigger/PlayerHitTriggerZone.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+
+namespace Assets.Scripts.Trigger
+{
+	public abstract class PlayerHitTriggerZone : MonoBehaviour
+	{
+		[SerializeField]
+		private string PlayerTag = "Player";
+
+
+		private void OnTriggerEnter2D(Collider2D collision)
+		{
+			if (collision.CompareTag(PlayerTag))
+			{
+				Debug.Log("Player hit trigger");
+				Trigger();
+			}
+		}
+
+		private void OnTriggerExit2D(Collider2D collision)
+		{
+			if (collision.CompareTag(PlayerTag))
+			{
+				Debug.Log("Player left trigger");
+				ExitTrigger();
+			}
+		}
+
+		protected abstract void Trigger();
+
+		protected abstract void ExitTrigger();
+	}
+}
