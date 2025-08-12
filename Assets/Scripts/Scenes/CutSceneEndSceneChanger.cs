0aee1f7644137bab3300eb407e964a1a213b96f3
Ben Alexander <ben250402@yahoo.co.uk>
2025-08-12 21:44:55 +0100

    4 Cutscene ending causes birthday p2 to start

---
+++ Assets/Scripts/Scenes/CutSceneEndSceneChanger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace Assets.Scripts.Scenes
+{
+	public class CutSceneEndSceneChanger : MonoBehaviour
+	{
+		[SerializeField]
+		private string sceneName;
+
+
+		public void ChangeScene()
+		{
+			SceneManager.LoadScene(sceneName);
+		} 
+	}
+}
