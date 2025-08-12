8cedf490c2e3864da8419318b7e6a92da359978f
Ben Alexander <ben250402@yahoo.co.uk>
2025-08-12 21:12:37 +0100

    4 Added running cutscene and speech to birthday p1

---
+++ Assets/Scripts/Trigger/SceneChangeTriggerZone.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace Assets.Scripts.Trigger
+{
+	public class SceneChangeTriggerZone : PlayerHitTriggerZone
+	{
+		[SerializeField]
+		private string sceneName;
+
+
+		protected override void Trigger()
+		{
+			SceneManager.LoadScene(sceneName);
+		}
+
+		protected override void ExitTrigger()
+		{
+			// Nothing
+		}
+	}
+}
