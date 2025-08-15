6ed4e57d8cb92e2d240696b14b7db397298ea97d
Ben Alexander <ben250402@yahoo.co.uk>
2025-08-15 07:30:45 +0100

    4 Added FadeToBlack at end of cutscene

---
+++ Assets/Scripts/UI/FadeToBlack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Assets.Scripts.UI
+{
+	public class FadeToBlack : MonoBehaviour
+	{
+		[SerializeField]
+		private Image image;
+
+		[SerializeField]
+		private float fadeDuration = 1f;
+
+
+		public void StartFade()
+		{
+			StartCoroutine(FadeAndChangeScene());
+		}
+
+
+		private IEnumerator FadeAndChangeScene()
+		{
+			image.gameObject.SetActive(true);
+
+			image.color = Color.black;
+			image.color.WithAlpha(0);
+
+			var elapsed = 0f;
+			var color = image.color;
+
+			while (elapsed < fadeDuration)
+			{
+				elapsed += Time.deltaTime;
+
+				color.a = Mathf.Clamp01(elapsed / fadeDuration);
+				image.color = color;
+
+				yield return null;
+			}
+		}
+	}
+}
