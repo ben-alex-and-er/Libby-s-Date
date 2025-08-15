1f2b72d2987484d44617e56eb2b079af13095f0d
Ben Alexander <ben250402@yahoo.co.uk>
2025-08-15 16:56:12 +0100

    5 Added music to birthday and dream scenes

---
+++ Assets/Scripts/Scenes/PersistInScenes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace Assets.Scripts.Scenes
+{
+	public class PersistInScenes : MonoBehaviour
+	{
+		[SerializeField]
+		private List<string> scenes;
+
+
+		void Awake()
+		{
+			if (!scenes.Contains(SceneManager.GetActiveScene().name))
+			{
+				Destroy(gameObject);
+				return;
+			}
+
+			DontDestroyOnLoad(gameObject);
+
+			SceneManager.sceneLoaded += RemoveOnInvalidScene;
+		}
+
+		private void RemoveOnInvalidScene(Scene scene, LoadSceneMode mode)
+		{
+			if (scenes.Contains(scene.name))
+				return;
+
+			SceneManager.sceneLoaded -= RemoveOnInvalidScene;
+
+			Destroy(gameObject);
+		}
+	}
+}
