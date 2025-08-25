6d32de7fb88ebf99f286262389d0e51abd549aa1
Ben Alexander <ben250402@yahoo.co.uk>
2025-08-25 10:16:53 +0100

    8 Added Credits

---
+++ Assets/Scripts/UI/CreditsScroller.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+public class CreditsScroller : MonoBehaviour
+{
+	public float scrollSpeed = 50f;
+
+
+	void Update()
+	{
+		transform.Translate(scrollSpeed * Time.deltaTime * Vector3.up);
+	}
+}
